ec739d40b0bce999f2537d80ed2f835c1e2d99c9
NanaKwame <easj.asante@gmail.com>
2014-01-15 10:21:48 -0800

    dummy app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrackIt::Application.config.secret_key_base = '9b074825b7afe83a88b77278fd18bd0dd90680ba52b422b7d10dbcae99e9a2caefb4f95a83c1699bab65c005a744ab31134b3b4d6b8f1249afb367464c81648d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CrackIt::Application.config.session_store :cookie_store, key: '_CrackIt_session'
