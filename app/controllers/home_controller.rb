1e75be79a35a7840712f4f3349eaf413718707b2
NanaKwame <easj.asante@gmail.com>
2014-01-20 10:49:39 -0800

    backend and basic front end of login, signup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+	before_action :check_logged_in, except: [:login]
 
 	def login
+	end
+
+	def dashboard
+	end
+
+	private
+	def check_logged_in
+		if current_user.nil?
+			redirect_to login_home_index_path
+		end
 	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ CrackIt::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'http://crackit.herokuapp.com' }
 end
